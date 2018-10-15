8ad760d358612607c7f3922fd91217deb3339355
drdh <drdh@mail.ustc.edu.cn>
2018-10-15 13:47:51 +0800

    reference

--- Cpp/test.cpp
+++ Cpp/test.cpp
@@ -1,10 +1,15 @@
 #include<iostream>
 #include<string>
+#include<vector>
 
 using namespace std;
 
 int main(){
-  cout<<"122\n"<<endl;
-  string a;
+  vector<int>v1;
+  for(int i=0;i<=10;i++)
+    v1.push_back(i);
+
+  for(auto i:v1)
+    cout<<i<<endl;
 }
 
